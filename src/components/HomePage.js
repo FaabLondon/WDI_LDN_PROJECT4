f0e4f90b1e0b7f0a05a03148db09d06753796a9d
Fabienne Prouteau <faabke@gmail.com>
2018-04-07 09:30:19 +0100

    Homepage finalised

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,32 +2,50 @@ import React from 'react';
 
 import '../scss/components/HomePage.scss';
 
-const HomePage = () => {
-  return(
-    <main>
-      <section className="hero is-medium homepage">
-        <h1 className="title is-size-3-desktop is-size-4-touch has-text-white has-text-centered">Get unlimited clothing for spring</h1>
-        <h4 className="subtitle is-size-4-desktop is-size-5-touch has-text-white has-text-centered">Refresh your wardrobe with high-end brands - Just rent them for a fraction of the retail price! </h4>
-      </section>
+class HomePage extends React.Component{
 
-      <section className="section">
-        <h1 className="title is-size-3-desktop is-size-4-touch has-text-white has-text-centered">The Renting Revolution Is Here</h1>
-        <div className="columns has-text-centered">
-          <div className="column"></div>
-          <div className="column is-one-third">
-            <h3 className="subtitle is-size-4-desktop is-size-5-touch has-text-grey">Lorem</h3>
-            <div className="content has-text-grey-light">lorem</div>
+  state = {
+    conceptPresentation: [{
+      icon: '',
+      subtitle: 'Fashion Freedom',
+      description: 'You’ve got 100,000s of options at your fingertips. Try new things, have more fun and go for it.'
+    }, {
+      icon: '',
+      subtitle: 'Total wardrobe Flexibility',
+      description: 'Let’s be real: your style, size and budget change over time. Now, your closet can too.'
+    }, {
+      icon: '',
+      subtitle: 'Smarter Closet',
+      description: 'Imagine a closet that has exactly what you want. Takes up no space. And does your dry cleaning.'
+    }, {
+      icon: '',
+      subtitle: 'Smaller Clothing Footprint',
+      description: 'Clothes end up in the back of closets or landfills. Power the sharing economy and rent instead.'
+    }]
+  };
+
+  render(){
+    return(
+      <main>
+        <section className="hero is-medium homepage">
+          <h1 className="title is-size-3-desktop is-size-4-touch has-text-white has-text-centered">Get unlimited clothing for spring</h1>
+          <h4 className="subtitle is-size-4-desktop is-size-5-touch has-text-white has-text-centered">Refresh your wardrobe with high-end brands - Just rent them for a fraction of the retail price! </h4>
+        </section>
+
+        <section className="section">
+          <h1 className="title is-size-3-desktop is-size-4-touch has-text-grey has-text-centered">The Renting Revolution Is Here</h1>
+          <div className="columns has-text-centered is-multiline">
+            {this.state.conceptPresentation.map((elt, i) =>
+              <div key={i} className="column is-half">
+                <h3 className="is-size-3-desktop is-size-6-touch has-text-grey">{elt.subtitle}</h3>
+                <p className="content has-text-grey-light">{elt.description}</p>
+              </div>
+            )}
           </div>
-          <div className="column"></div>
-          <div className="column is-one-third">
-            <h3 className="subtitle is-size-4-desktop is-size-5-touch has-text-grey">lorem</h3>
-            <div className="content has-text-grey-light">lorem </div>
-          </div>
-          <div className="column"></div>
-        </div>
-      </section>
-    </main>
-  );
-};
+        </section>
+      </main>
+    );
+  }
+}
 
 export default HomePage;
