ae85236e68b64a5f1808176e612747061f1620bc
Fabienne Prouteau <faabke@gmail.com>
2018-04-11 14:30:51 +0100

    added tests for index and show routes - server side

---
+++ test/server/items/index_spec.js
@@ -0,0 +1,100 @@
+/* global api, describe, it, expect, beforeEach */
+const { Item } = require('../../../models/item');
+const User = require('../../../models/user');
+
+const itemData = [{
+  brand: 'Lanvin',
+  shortDescription: 'Blue satin trench coat',
+  longDescription: 'Epaulettes, storm flaps, side pockets, belt loops, detachable waist tie, button-fastening cuff tabs, back vent, fully lined',
+  retailPrice: 1855,
+  rentalPrice: 40,
+  category: 'Clothing',
+  type: 'JacketsCoats',
+  occasion: ['Formal', 'Daytime', 'Work'],
+  colors: ['Blue'],
+  sizeAvailable: 'M',
+  mainImage: 'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/663986_blue_1.jpg',
+  smallImages: ['https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/663986_blue_5.jpg', 'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/663986_blue_4.jpg', 'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/663986_blue_3.jpg'],
+  available: true
+}, {
+  brand: 'Self-Portrait',
+  shortDescription: 'Navy embroidered tulle maxi dress',
+  longDescription: 'Textured, embroidered, black grosgrain tie-fastening shoulders, metallic weave, wrap-effect front, lace insert at waist, pleated, tiered, almond lining',
+  retailPrice: 390,
+  rentalPrice: 25,
+  category: 'Clothing',
+  type: 'Dresses',
+  occasion: ['Formal', 'Party'],
+  colors: ['Navy', 'Blue'],
+  sizeAvailable: 'M',
+  mainImage: 'https://m.hng.io/catalog/product/6/6/665878_navy_and_other_4.jpg',
+  smallImages: ['https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/665878_navy_and_other_5.jpg', 'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/665878_navy_and_other_2.jpg',  'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/665878_navy_and_other_3.jpg'],
+  available: true
+}];
+
+describe('GET /items', () => {
+  beforeEach(done => {
+    Promise.all([
+      User.remove({}),
+      Item.remove({})
+    ])
+      .then(() => Item.create(itemData))
+      .then(() => done());
+  });
+
+  it('should return a 200 response', done => {
+    api
+      .get('/api/items')
+      .expect(200, done);
+  });
+
+  it('should return an array of items', done => {
+    api
+      .get('/api/items')
+      .end((err, res) => {
+        expect(res.body).to.be.an('array');
+        res.body.forEach(item => {
+          expect(item).to.include.keys([
+            'brand',
+            'shortDescription',
+            'longDescription',
+            'retailPrice',
+            'rentalPrice',
+            'category',
+            'type',
+            'occasion',
+            'colors',
+            'sizeAvailable',
+            'mainImage',
+            'smallImages',
+            'available'
+          ]);
+        });
+      });
+    done();
+  });
+
+  it('should return the correct data', done => {
+    api
+      .get('/api/items')
+      .end((err, res) => {
+        res.body = res.body.sort((a, b) => a.name > b.name);
+        res.body.forEach((item, i) => {
+          expect(item.brand).to.eq(itemData[i].brand);
+          expect(item.shortDescription).to.eq(itemData[i].shortDescription);
+          expect(item.longDescription).to.eq(itemData[i].longDescription);
+          expect(item.retailPrice).to.eq(itemData[i].retailPrice);
+          expect(item.rentalPrice).to.eq(itemData[i].rentalPrice);
+          expect(item.category).to.eq(itemData[i].category);
+          expect(item.type).to.eq(itemData[i].type);
+          expect(item.occasion).to.deep.eq(itemData[i].occasion);
+          expect(item.colors).to.deep.eq(itemData[i].colors);
+          expect(item.sizeAvailable).to.eq(itemData[i].sizeAvailable);
+          expect(item.mainImage).to.eq(itemData[i].mainImage);
+          expect(item.smallImages).to.deep.eq(itemData[i].smallImages);
+          expect(item.available).to.eq(itemData[i].available);
+        });
+        done();
+      });
+  });
+});
---
+++ test/server/items/show_spec.js
@@ -0,0 +1,97 @@
+/* global api, describe, it, expect, beforeEach */
+const { Item } = require('../../../models/item');
+const User = require('../../../models/user');
+
+const itemData = [{
+  brand: 'Lanvin',
+  shortDescription: 'Blue satin trench coat',
+  longDescription: 'Epaulettes, storm flaps, side pockets, belt loops, detachable waist tie, button-fastening cuff tabs, back vent, fully lined',
+  retailPrice: 1855,
+  rentalPrice: 40,
+  category: 'Clothing',
+  type: 'JacketsCoats',
+  occasion: ['Formal', 'Daytime', 'Work'],
+  colors: ['Blue'],
+  sizeAvailable: 'M',
+  mainImage: 'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/663986_blue_1.jpg',
+  smallImages: ['https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/663986_blue_5.jpg', 'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/663986_blue_4.jpg', 'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/663986_blue_3.jpg'],
+  available: true
+}, {
+  brand: 'Self-Portrait',
+  shortDescription: 'Navy embroidered tulle maxi dress',
+  longDescription: 'Textured, embroidered, black grosgrain tie-fastening shoulders, metallic weave, wrap-effect front, lace insert at waist, pleated, tiered, almond lining',
+  retailPrice: 390,
+  rentalPrice: 25,
+  category: 'Clothing',
+  type: 'Dresses',
+  occasion: ['Formal', 'Party'],
+  colors: ['Navy', 'Blue'],
+  sizeAvailable: 'M',
+  mainImage: 'https://m.hng.io/catalog/product/6/6/665878_navy_and_other_4.jpg',
+  smallImages: ['https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/665878_navy_and_other_5.jpg', 'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/665878_navy_and_other_2.jpg',  'https://m.hng.io/catalog/product/cache/1/gallery/390x550/0dc2d03fe217f8c83829496872af24a0/6/6/665878_navy_and_other_3.jpg'],
+  available: true
+}];
+
+describe('GET /items/:id', () => {
+  let item = {};
+  beforeEach(done => {
+    Promise.all([
+      User.remove({}),
+      Item.remove({})
+    ])
+      .then(() => Item.create(itemData))
+      .then(bangers => item = bangers[0]) //1st banger returned
+      .then(() => done());
+  });
+
+  it('should return a 200 response', done => {
+    api
+      .get(`/api/items/${item._id}`)
+      .expect(200, done);
+  });
+
+  it('should return one item', done => {
+    api
+      .get(`/api/items/${item._id}`)
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.include.keys([
+          'brand',
+          'shortDescription',
+          'longDescription',
+          'retailPrice',
+          'rentalPrice',
+          'category',
+          'type',
+          'occasion',
+          'colors',
+          'sizeAvailable',
+          'mainImage',
+          'smallImages',
+          'available'
+        ]);
+      });
+    done();
+  });
+
+  it('should return the correct data', done => {
+    api
+      .get(`/api/items/${item._id}`)
+      .end((err, res) => {
+        expect(res.body.brand).to.eq(itemData[0].brand);
+        expect(res.body.shortDescription).to.eq(itemData[0].shortDescription);
+        expect(res.body.longDescription).to.eq(itemData[0].longDescription);
+        expect(res.body.retailPrice).to.eq(itemData[0].retailPrice);
+        expect(res.body.rentalPrice).to.eq(itemData[0].rentalPrice);
+        expect(res.body.category).to.eq(itemData[0].category);
+        expect(res.body.type).to.eq(itemData[0].type);
+        expect(res.body.occasion).to.deep.eq(itemData[0].occasion);
+        expect(res.body.colors).to.deep.eq(itemData[0].colors);
+        expect(res.body.sizeAvailable).to.eq(itemData[0].sizeAvailable);
+        expect(res.body.mainImage).to.eq(itemData[0].mainImage);
+        expect(res.body.smallImages).to.deep.eq(itemData[0].smallImages);
+        expect(res.body.available).to.eq(itemData[0].available);
+        done();
+      });
+  });
+});
